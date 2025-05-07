32cd4180c3300b8542ad3eb13ef0bab89ef2cfbb
coolguy284 <coolguy284v64@gmail.com>
2025-05-07 04:49:18 -0400

    parse path in ClientRequest

--- src/main.mjs
+++ src/main.mjs
@@ -1,14 +1,42 @@
 import { createServer as createHTTPServer } from 'node:http';
+import { createServer as createHTTPSServer } from 'node:https';
 import { createServer as createHTTP2Server } from 'node:http2';
 
 class ClientRequest {
+  path; // string<URL pathname, no search params> | null
+  pathSearchParams; // URLSearchParams (like Map<string<key>, string<value>>, with duplicate key support) | null
+  pathRaw; // string<URL pathname>
+  headers;
   
+  constructor({ pathString }) {
+    this.pathRaw = pathString;
+    
+    try {
+      const url = new URL(`https://domain/${pathString}`);
+      this.path = url.pathname;
+      this.pathSearchParams = url.searchParams;
+    } catch { /* empty */ }
+  }
 }
 
 export class Server {
   #instances;
   #requestListener;
   #listening = false;
+  
+  async #handleHTTP1Request(req, res) {
+    await this.#requestListener(new ClientRequest({
+      pathString: req.url,
+    }));
+  }
+  
+  async #handleHTTP1Upgrade(req, res) {
+    
+  }
+  
+  async #handleHTTP2Request(req, res) {
+    
+  }
   
   /*
     instances:
@@ -28,16 +56,70 @@ export class Server {
     instances,
     requestListener,
   }) {
-    this.#instances = instances;
+    this.#instances = instances.map(
+      ({ mode, ip, port, cert, key, options }) => ({
+        mode,
+        ip,
+        port,
+        cert,
+        key,
+        options,
+        server: null,
+      })
+    );
+    
+    for (let instance of this.#instances) {
+      const { mode, cert, key, options } = instance;
+      
+      switch (mode) {
+        case 'http':
+          if (typeof options != 'object' && options != undefined) {
+            throw new Error(`options not object or undefined: ${options}`);
+          }
+          
+          instance.server = createHTTPServer(options);
+          
+          instance.server.on('request', async (req, res) => {
+            await this.#handleHTTP1Request(req, res);
+          });
+          
+          instance.server.on('upgrade', async (req, res) => {
+            await this.#handleHTTP1Upgrade(req, res);
+          });
+          break;
+      }
+    }
+    
     this.#requestListener = requestListener;
   }
   
   listen() {
-    // TODO
+    for (const { mode, ip, port } of this.#instances) {
+      switch (mode) {
+        case 'http':
+        case 'https':
+        case 'http2':
+          this.#instances.server.listen(port, ip);
+          break;
+        
+        default:
+          throw new Error(`unknown or unsupported mode: ${mode}`);
+      }
+    }
+    
+    this.#listening = true;
   }
   
   close() {
-    // TODO
+    if (!this.#listening) {
+      throw new Error('cannot close servers, servers not listening yet');
+    }
+    
+    for (const { server } of this.#instances) {
+      this.#instances.server.closeAllConnections();
+    }
+    
+    this.#listening = false;
   }
 }
 
