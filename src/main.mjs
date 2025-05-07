83167e395df4461b56156463781e428e5d1a36d4
coolguy284 <coolguy284v64@gmail.com>
2025-05-07 05:26:59 -0400

    implement some parts of clientrequest

--- src/main.mjs
+++ src/main.mjs
@@ -1,21 +1,49 @@
 import { createServer as createHTTPServer } from 'node:http';
 import { createServer as createHTTPSServer } from 'node:https';
-import { createServer as createHTTP2Server } from 'node:http2';
+import { createSecureServer as createHTTP2Server } from 'node:http2';
 
 class ClientRequest {
-  path; // string<URL pathname, no search params> | null
-  pathSearchParams; // URLSearchParams (like Map<string<key>, string<value>>, with duplicate key support) | null
+  path = null; // string<URL pathname, no search params> | null
+  pathSearchParams = null; // URLSearchParams (like Map<string<key>, string<value>>, with duplicate key support) | null
   pathRaw; // string<URL pathname>
-  headers;
+  headers; // Map<string, string> (http2-like headers object, with ':path' removed)
+  /*
+    {
+      mode: 'http' | 'https' | 'http2' | 'http3',
+      if mode == 'http' | 'https':
+        req: IncomingMessage,
+        res: ServerResponse,
+    }
+  */
+  internal;
   
-  constructor({ pathString }) {
+  constructor({
+    pathString,
+    headers,
+    internal,
+  }) {
     this.pathRaw = pathString;
     
+    let pathUrl;
     try {
-      const url = new URL(`https://domain/${pathString}`);
-      this.path = url.pathname;
-      this.pathSearchParams = url.searchParams;
+      pathUrl = new URL(`https://domain/${pathString}`);
     } catch { /* empty */ }
+    
+    if (pathUrl != null) {
+      this.path = pathUrl.pathname;
+      this.pathSearchParams = pathUrl.searchParams;
+    }
+    
+    this.headers = headers;
+    this.internal = internal;
+  }
+  
+  pathMatch(pathStart) {
+    // TODO
+  }
+  
+  subRequest(pathStart) {
+    // TODO
   }
 }
 
@@ -25,17 +53,29 @@ export class Server {
   #listening = false;
   
   async #handleHTTP1Request(req, res) {
+    let headers = Object.fromEntries(Object.entries(req.headers));
+    
+    headers[':scheme'] = 'http';
+    headers[':method'] = req.method;
+    headers[':authority'] = req.headers.host;
+    delete headers.host;
+    
     await this.#requestListener(new ClientRequest({
       pathString: req.url,
+      headers,
+      internal: {
+        req,
+        res,
+      },
     }));
   }
   
-  async #handleHTTP1Upgrade(req, res) {
-    
+  async #handleHTTP1Upgrade(req, head, socket) {
+    // TODO
   }
   
-  async #handleHTTP2Request(req, res) {
-    
+  async #handleHTTP2Request(req) {
+    // TODO
   }
   
   /*
@@ -93,19 +133,36 @@ export class Server {
     this.#requestListener = requestListener;
   }
   
-  listen() {
-    for (const { mode, ip, port } of this.#instances) {
-      switch (mode) {
-        case 'http':
-        case 'https':
-        case 'http2':
-          this.#instances.server.listen(port, ip);
-          break;
-        
-        default:
-          throw new Error(`unknown or unsupported mode: ${mode}`);
-      }
-    }
+  async listen() {
+    await Promise.all(
+      this.#instances.map(async ({ mode, ip, port, server }) => {
+        switch (mode) {
+          case 'http':
+          case 'https':
+          case 'http2':
+            await new Promise((r, j) => {
+              const successListener = () => {
+                r();
+                server.off('error', errorListener);
+              };
+              
+              const errorListener = err => {
+                j(err);
+              };
+              
+              server.on('error', errorListener);
+              
+              server.listen(port, ip, () => {
+                successListener();
+              });
+            });
+            break;
+          
+          default:
+            throw new Error(`unknown or unsupported mode: ${mode}`);
+        }
+      })
+    );
     
     this.#listening = true;
   }
@@ -116,7 +173,7 @@ export class Server {
     }
     
     for (const { server } of this.#instances) {
-      this.#instances.server.closeAllConnections();
+      server.closeAllConnections();
     }
     
     this.#listening = false;
