59fa527cd9d4892a5c91b6bd3a41dced6b485fa9
coolguy284 <coolguy284v64@gmail.com>
2025-05-09 00:40:41 -0400

    add ciphers to server test

--- test/basic/main.mjs
+++ test/basic/main.mjs
@@ -1,3 +1,4 @@
+import { constants } from 'node:crypto';
 import { readFile } from 'node:fs/promises';
 
 import { WebSocketServer } from 'ws';
@@ -31,11 +32,29 @@ const server = new Server({
       ip: 'localhost',
       port: 8443,
       options: {
+        secureOptions:
+          constants.SSL_OP_NO_SSLv2 |
+          constants.SSL_OP_NO_SSLv3 |
+          constants.SSL_OP_NO_TLSv1 |
+          constants.SSL_OP_NO_TLSv1_1,
+        
+        ciphers: [
+          'TLS_AES_256_GCM_SHA384',
+          'TLS_CHACHA20_POLY1305_SHA256',
+          'TLS_AES_128_GCM_SHA256',
+          'TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256',
+          'TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384',
+          'TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256',
+          'TLS_ECDHE_RSA_WITH_ARIA_256_GCM_SHA384',
+          'TLS_ECDHE_RSA_WITH_ARIA_128_GCM_SHA256',
+          '@STRENGTH',
+        ].join(':'),
+        
         cert: await readFile('cert.pem'),
         key: await readFile('key.pem'),
       },
     },
-    /*{
+    {
       listenerID: 'http2',
       mode: 'http2',
       ip: 'localhost',
@@ -47,7 +66,7 @@ const server = new Server({
         enableConnectProtocol: true,
       },
     },
-    {
+    /*{
       listenerID: 'http3',
       mode: 'http3',
       ip: 'localhost',
