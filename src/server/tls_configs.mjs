aa55bd4b30f3e9e6ea8003a6ae8059f65fa1cb68
coolguy284 <coolguy284v64@gmail.com>
2025-05-21 16:09:42 -0400

    add commented out more secure tls

--- src/server/tls_configs.mjs
+++ src/server/tls_configs.mjs
@@ -20,3 +20,25 @@ export const TLS_CONFIG_NORMAL = {
     '@STRENGTH',
   ].join(':'),
 };
+
+/*export const TLS_CONFIG_SECURE = {
+  secureOptions:
+    constants.SSL_OP_NO_SSLv2 |
+    constants.SSL_OP_NO_SSLv3 |
+    constants.SSL_OP_NO_TLSv1 |
+    constants.SSL_OP_NO_TLSv1_1 |
+    constants.SSL_OP_NO_TLSv1_2,
+  
+  ciphers: [
+    'TLS_AES_256_GCM_SHA384',
+    'TLS_CHACHA20_POLY1305_SHA256',
+    'TLS_AES_128_GCM_SHA256',
+    'TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256',
+    'TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384',
+    'TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256',
+    'TLS_ECDHE_RSA_WITH_ARIA_256_GCM_SHA384',
+    'TLS_ECDHE_RSA_WITH_ARIA_128_GCM_SHA256',
+    '@STRENGTH',
+    '@SECLEVEL=5',
+  ].join(':'),
+};*/
