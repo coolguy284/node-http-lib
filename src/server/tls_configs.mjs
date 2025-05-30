c41ce7f6c8cc6545d521b46bd492cdea0ac451ca
coolguy284 <coolguy284v64@gmail.com>
2025-05-30 02:56:33 -0400

    continue tweaking tls configs

--- src/server/tls_configs.mjs
+++ src/server/tls_configs.mjs
@@ -1,6 +1,8 @@
 import { constants } from 'node:crypto';
 
 // passes the Qualys SSL Labs test
+// also requires sending header:
+// 'strict-transport-security': 'max-age=31536000', // 1 year
 export const TLS_CONFIG_NORMAL = {
   secureOptions:
     constants.SSL_OP_NO_SSLv2 |
@@ -12,6 +14,10 @@ export const TLS_CONFIG_NORMAL = {
     constants.defaultCoreCipherList, // contains many ciphers separated by ':' iirc
     '!TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256',
     '!TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384',
+    '!TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384',
+    '!TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA',
+    '!TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256',
+    '!TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA',
     '!TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA',
     '!TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA',
     '!TLS_RSA_WITH_AES_256_GCM_SHA384',
@@ -30,7 +36,28 @@ export const TLS_CONFIG_NORMAL = {
   ].join(':'),
 };
 
-export const TLS_CONFIG_SECURE = {
+export const TLS_CONFIG_MORE_SECURE = {
+  secureOptions:
+    constants.SSL_OP_NO_SSLv2 |
+    constants.SSL_OP_NO_SSLv3 |
+    constants.SSL_OP_NO_TLSv1 |
+    constants.SSL_OP_NO_TLSv1_1 |
+    constants.SSL_OP_NO_TLSv1_2,
+  
+  ciphers: [
+    'TLS_AES_256_GCM_SHA384',
+    'TLS_CHACHA20_POLY1305_SHA256',
+    'TLS_AES_128_GCM_SHA256',
+    'TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256',
+    'TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384',
+    'TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256',
+    'TLS_ECDHE_RSA_WITH_ARIA_256_GCM_SHA384',
+    'TLS_ECDHE_RSA_WITH_ARIA_128_GCM_SHA256',
+    '@STRENGTH',
+  ].join(':'),
+};
+
+export const TLS_CONFIG_MORE_SECURE_2 = {
   secureOptions:
     constants.SSL_OP_NO_SSLv2 |
     constants.SSL_OP_NO_SSLv3 |
