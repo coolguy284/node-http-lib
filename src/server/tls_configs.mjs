7804e902b7e5725c7ca82751cc836effbd3fe4af
coolguy284 <coolguy284v64@gmail.com>
2025-05-30 02:54:15 -0400

    start improving ssl for ssllabs

--- src/server/tls_configs.mjs
+++ src/server/tls_configs.mjs
@@ -9,19 +9,28 @@ export const TLS_CONFIG_NORMAL = {
     constants.SSL_OP_NO_TLSv1_1,
   
   ciphers: [
-    'TLS_AES_256_GCM_SHA384',
-    'TLS_CHACHA20_POLY1305_SHA256',
-    'TLS_AES_128_GCM_SHA256',
-    'TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256',
-    'TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384',
-    'TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256',
-    'TLS_ECDHE_RSA_WITH_ARIA_256_GCM_SHA384',
-    'TLS_ECDHE_RSA_WITH_ARIA_128_GCM_SHA256',
+    constants.defaultCoreCipherList, // contains many ciphers separated by ':' iirc
+    '!TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256',
+    '!TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384',
+    '!TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA',
+    '!TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA',
+    '!TLS_RSA_WITH_AES_256_GCM_SHA384',
+    '!TLS_RSA_WITH_AES_256_CCM_8',
+    '!TLS_RSA_WITH_AES_256_CCM',
+    '!TLS_RSA_WITH_ARIA_256_GCM_SHA384',
+    '!TLS_RSA_WITH_AES_128_GCM_SHA256',
+    '!TLS_RSA_WITH_AES_128_CCM_8',
+    '!TLS_RSA_WITH_AES_128_CCM',
+    '!TLS_RSA_WITH_ARIA_128_GCM_SHA256',
+    '!TLS_RSA_WITH_AES_256_CBC_SHA256',
+    '!TLS_RSA_WITH_AES_128_CBC_SHA256',
+    '!TLS_RSA_WITH_AES_256_CBC_SHA',
+    '!TLS_RSA_WITH_AES_128_CBC_SHA',
     '@STRENGTH',
   ].join(':'),
 };
 
-/*export const TLS_CONFIG_SECURE = {
+export const TLS_CONFIG_SECURE = {
   secureOptions:
     constants.SSL_OP_NO_SSLv2 |
     constants.SSL_OP_NO_SSLv3 |
@@ -41,4 +50,4 @@ export const TLS_CONFIG_NORMAL = {
     '@STRENGTH',
     '@SECLEVEL=5',
   ].join(':'),
-};*/
+};
