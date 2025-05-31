901c4b62c68dbfbe0c4031ccc38a3f1998d87599
coolguy284 <coolguy284v64@gmail.com>
2025-05-31 05:22:40 -0400

    export tls configs

--- src/main.mjs
+++ src/main.mjs
@@ -9,3 +9,8 @@ export { Server } from './server/server.mjs';
 export { serveFolder } from './server/serve_folder.mjs';
 export { serveFile } from './server/serve_file.mjs';
 export { serveWebSocket } from './server/serve_ws.mjs';
+export {
+  TLS_CONFIG_NORMAL,
+  TLS_CONFIG_MORE_SECURE_1,
+  TLS_CONFIG_MORE_SECURE_2,
+} from './server/tls_configs.mjs';
