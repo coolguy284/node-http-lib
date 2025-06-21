89914cc1b33d0b076b085019bf1c6de6e3483a9a
coolguy284 <coolguy284v64@gmail.com>
2025-06-20 22:46:44 -0400

    server test all proxy http protocols

--- test/server_basic/main.mjs
+++ test/server_basic/main.mjs
@@ -18,8 +18,6 @@ wsServer.on('connection', ws => {
   });
 });
 
-const PROXY_TARGET_INSTANCE = INSTANCES[0];
-
 await using server = new Server({
   instances: INSTANCES,
   requestListener: async serverRequest => {
@@ -49,26 +47,53 @@ await using server = new Server({
       });
     } else if (serverRequest.path == 'file.txt') {
       serverRequest.respond(`plain text ${new Date().toISOString()}`);
-    } else if (serverRequest.pathMatch('proxy_all/')) {
-      await serveProxy({
-        serverRequest: serverRequest.subRequest('proxy_all/'),
-        requestParameters: {
-          mode: PROXY_TARGET_INSTANCE.mode,
-          host: PROXY_TARGET_INSTANCE.ip,
-          port: PROXY_TARGET_INSTANCE.port,
-        },
-      });
     } else if (serverRequest.pathMatch('proxy_files/')) {
       await serveProxy({
         serverRequest: serverRequest.subRequest('proxy_files/'),
         requestParameters: {
-          mode: PROXY_TARGET_INSTANCE.mode,
-          host: PROXY_TARGET_INSTANCE.ip,
-          port: PROXY_TARGET_INSTANCE.port,
+          mode: INSTANCES[0].mode,
+          host: INSTANCES[0].ip,
+          port: INSTANCES[0].port,
           pathPrefix: 'files/',
         },
       });
-    } else {
+    } else if (serverRequest.pathMatch('proxy_all_http/')) {
+      await serveProxy({
+        serverRequest: serverRequest.subRequest('proxy_all_http/'),
+        requestParameters: {
+          mode: INSTANCES[0].mode,
+          host: INSTANCES[0].ip,
+          port: INSTANCES[0].port,
+        },
+      });
+    } else if (serverRequest.pathMatch('proxy_all_https/')) {
+      await serveProxy({
+        serverRequest: serverRequest.subRequest('proxy_all_https/'),
+        requestParameters: {
+          mode: INSTANCES[1].mode,
+          host: INSTANCES[1].ip,
+          port: INSTANCES[1].port,
+        },
+      });
+    } else if (serverRequest.pathMatch('proxy_all_http2/')) {
+      await serveProxy({
+        serverRequest: serverRequest.subRequest('proxy_all_http2/'),
+        requestParameters: {
+          mode: INSTANCES[2].mode,
+          host: INSTANCES[2].ip,
+          port: INSTANCES[2].port,
+        },
+      });
+    } /*else if (serverRequest.pathMatch('proxy_all_http3/')) {
+      await serveProxy({
+        serverRequest: serverRequest.subRequest('proxy_all_http3/'),
+        requestParameters: {
+          mode: INSTANCES[3].mode,
+          host: INSTANCES[3].ip,
+          port: INSTANCES[3].port,
+        },
+      });
+    }*/ else {
       serverRequest.respond('Error: path not found', { ':status': 404 });
     }
   },
